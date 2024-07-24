5f7fb30b89c5e78aebe297cfe2de1a8e446f4407
chinmay saraswat <226301072@gkv.ac.in>
2024-07-24 20:01:24 +0530

    add sign-up page

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,20 @@
+'use client'
+import { useSession, signIn, signOut } from "next-auth/react"
+
+export default function Component() {
+  const { data: session } = useSession()
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    )
+  }
+  return (
+    <>
+      Not signed in <br />
+      <button className="bg-blue-800 px-3 py-1 border m-4 border-white rounded-md" onClick={() => signIn()}>Sign in</button>
+    </>
+  )
+}
