821a3ed77e1e2ca2c5c622d53af40b40adf52180
chinmay saraswat <226301072@gkv.ac.in>
2024-07-25 17:45:08 +0530

    add OTP verification and unique username

---
+++ src/app/api/verify-code/route.ts
@@ -0,0 +1,50 @@
+import dbConnect from "@/lib/dbconnect";
+import UserModel from "@/model/User";
+
+export async function POST(request:Request){
+    await dbConnect();
+
+    try {
+        const {username,code}=await request.json();
+        const decodedUsername =  decodeURIComponent(username)
+        const user = await UserModel.findOne({username:decodedUsername})
+
+        if(!user){
+            return Response.json(
+                {success:false,message:"user not found"},
+                {status:404}
+            )
+        }
+        // Check if the code is correct and not expired
+        const isCodeValid = user.verifyCode===code;
+        const isCodeNotExpired=new Date(user.verifyCodeExpiry)>new Date();
+
+        if(isCodeValid&&isCodeNotExpired){
+            user.isVerified=true;
+            await user.save();
+            return Response.json(
+                { success: true, message: 'Account verified successfully' },
+                { status: 200 }
+              );
+        }else if(!isCodeNotExpired){
+            //code is expired
+            return Response.json({
+                success:false,
+                message:"verification code has expired. Please sign up again to get a new code."
+            },{status:400});
+        }else{
+             // Code is incorrect
+      return Response.json(
+        { success: false, message: 'Incorrect verification code' },
+        { status: 400 }
+         );
+        }
+
+    }  catch (error) {
+        console.error('Error verifying user:', error);
+        return Response.json(
+          { success: false, message: 'Error verifying user' },
+          { status: 500 }
+        );
+      }
+    }
